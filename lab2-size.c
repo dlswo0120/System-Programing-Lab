34cc5fd596a29e62315dc40b17198e7e11b74400
dlswo0120 <54271764+dlswo0120@users.noreply.github.com>
2022-09-18 14:45:45 +0900

    Update lab2-size.c

--- lab2-size.c
+++ lab2-size.c
@@ -1,3 +1,11 @@
+/*lab02
+
+lseek() 시스템 호출을 사용하여, 파일의 원하는 부분을 출력하도록 만든다 : 
+테스트용 입력 파일“test.txt”에 “abcdefghijklmnop”를 저장한다
+lseek 를 사용하여 파일의 시작에서 10개의 character를 건너뛴 후 부터의 내용을 출력한다
+lseek 를 사용하여 파일의 끝에서 앞쪽으로 5개의 character를 건너뛴 후 그 내용을 출력한다*/
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
