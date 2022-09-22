b68ed36260543dc171478740bbe77020022279aa
dlswo0120 <54271764+dlswo0120@users.noreply.github.com>
2022-09-22 22:49:34 +0900

    Update lab3-stat.c

--- lab3-stat.c
+++ lab3-stat.c
@@ -1,3 +1,11 @@
+/*<lab03>
+
+stat() 시스템 호출을 이용하여 파일의 정보를 출력한다 : 
+주어진 하나의 파일의 inode 번호를 출력한다
+해당 파일의 처음 생성 날짜/시간, 마지막으로 업데이트 된 날짜/시간, 마지막으로 접근한 날짜/시간을 출력한다
+
+ */
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
