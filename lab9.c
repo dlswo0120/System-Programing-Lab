2dfe520145fec8ad59d4da7a46f2f359048003b7
dlswo0120 <54271764+dlswo0120@users.noreply.github.com>
2022-10-27 22:49:29 +0900

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,3 +1,15 @@
+/*
+<lab9>
+
+Fork()와 wait()를 이용하여 다음의 프로그램을 작성하라:
+부모 프로세스는 두 개의 자식 프로세스를 생성한다: Child 1, Child 2
+각각의 자식 프로세스는 execl()을 이용하여 “echo” 커맨드를 실행하면서 “This is Child 1(또는 Child 2)”를 출력한다 
+부모 프로세스는 wait() 를 실행하며 자식 프로세스들이 끝나기를 기다린다. Wait()를 실행하기 전 “Parent: Waiting for children” 하고 출력한다 
+자식 프로세스들이 끝나면 부모는 “Parent: All Children terminated” 하고 출력한다
+
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
