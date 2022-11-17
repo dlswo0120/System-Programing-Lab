a55ed7ad35bc69fd73139e67f36910398007ef0d
dlswo0120 <54271764+dlswo0120@users.noreply.github.com>
2022-11-17 23:07:15 +0900

    Update lab13.c

--- lab13.c
+++ lab13.c
@@ -1,3 +1,11 @@
+/*
+<lab13>
+
+Pipe(), fork(), execlp() system call 들을 사용하여 “ps –ef | grep telnet”의 동작을 구현한다: 
+부모 프로세스는 파이프를 통해서 “ps –ef” 명령 결과를 출력한다
+자식 프로세스는 부모 프로세스가 출력한 결과를 받아 “grep telnet” 명령을 수행한다
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
