215d8efc72d1319e19306a26cb3fa9896e12be3d
dlswo0120 <54271764+dlswo0120@users.noreply.github.com>
2022-11-30 00:18:01 +0900

    Update lab16-clnt.c

--- lab16-clnt.c
+++ lab16-clnt.c
@@ -1,3 +1,10 @@
+/*
+<lab16>
+Socket() 시스템 호출을 이용하여 간단한 메시지를 교환하는 client/server 프로그램을 작성하라 :
+Domain은 PF_UNIX, socket type은 SOCK_DGRAM, 프로토콜은 0을 이용
+교환할 간단한 메시지는 “This is a message from the client”
+서버 프로그램은 다음 페이지에 나와 있음
+Client 프로그램을 작성할 것*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
